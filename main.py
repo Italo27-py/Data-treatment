26f21d98decaf7cc41ae0b97f8e53d70db19fde1
Italo Reis <italodaisreis27@gmail.com>
2026-05-24 16:43:06 -0300

    Adicionando dados_asimov ao projeto

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import pandas as pd
-pd.read_csv("Dados_asimov_corrompido.xlsx - Campe Supply.csv")
+import seaborn as sns
+import statistics as sts
+
+#Data import
+dataset=pd.read_csv("Dados_asimov_corrompido.xlsx - Campe Supply.csv")
+#view
+print(dataset.head())
