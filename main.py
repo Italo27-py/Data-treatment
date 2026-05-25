d8ba1cb3b420f4af80ca2336580ae77333609564
Italo Reis <italodaisreis27@gmail.com>
2026-05-25 16:02:51 -0300

    Novo arquivo csv criado

--- main.py
+++ main.py
@@ -181,3 +181,6 @@ dataset = dataset[colunas_ordenadas]
 
 print(dataset.head(50).to_string())
 
+dataset.to_csv(r"D:\pyhton\projeto\dados_tratados.csv", index=False)
+
+print("Arquivo dados_tratados.csv criado com sucesso!")
